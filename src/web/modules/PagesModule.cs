ae8e3f0943fbe073da6084bc9a8ed1e0685f5dfb
ndelfour <ndelfour@fastbooking.net>
2019-12-09 22:30:11 +0100

    Add Template

---
+++ src/web/modules/PagesModule.cs
@@ -0,0 +1,25 @@
+﻿using Nancy;
+
+namespace NancyBootstrapAdmin2.Modules
+{
+    public class PagesModule : NancyModule
+    {
+        public PagesModule()
+        {
+            Get("/", _=> View["home.html"]);
+            Get("/404", _ => View["404.html"]);
+            Get("/Login", _ => View["login.html"]);
+            Get("/Register", _ => View["register.html"]);
+            Get("/forgot-password", _ => View["forgot-password.html"]);
+            Get("/blank", _ => View["blank.html"]);
+            Get("/Buttons", _ => View["buttons.html"]);
+            Get("/Cards", _ => View["cards.html"]);
+            Get("/Charts", _ => View["charts.html"]);
+            Get("/Tables", _ => View["tables.html"]);
+            Get("/utilities-color", _ => View["utilities-color.html"]);
+            Get("/utilities-border", _ => View["utilities-border.html"]);
+            Get("/utilities-animation", _ => View["utilities-animation.html"]);
+            Get("/utilities-other", _ => View["utilities-other.html"]);
+        }
+    }
+}
---
+++ src/web/startup.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Nancy;
+using Nancy.Conventions;
+using Nancy.Owin;
+
+namespace NancyBootstrapAdmin2
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<KestrelServerOptions>(options =>
+            {
+                options.AllowSynchronousIO = true;
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseOwin(b=>b.UseNancy());
+        }
+    }
+
+    public class CustomBootstrapper : DefaultNancyBootstrapper
+    {
+        protected override void ConfigureConventions(NancyConventions nancyConventions)
+        {
+            base.ConfigureConventions(nancyConventions);
+
+            nancyConventions.StaticContentsConventions.AddDirectory("Content", "Content");
+        }
+    }
+}
